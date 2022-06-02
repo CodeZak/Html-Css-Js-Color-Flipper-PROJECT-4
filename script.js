5ed5d20d891c0215621d10cc2f737275634d9601
Zakariae Mahi <105796338+Zakitek@users.noreply.github.com>
2022-06-02 16:33:26 +0000

    ..

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getRndInteger(min, max) {
   
 function colors() {
 const colors = ["#F15025", "Green", "Rgba(133,122,200)","Red"];
-let x = getRndInteger(0, 3);
+let x = getRndInteger(0, colors.length-1);
 let chosen_color = colors[x]
 document.getElementById("color_code").innerHTML = chosen_color;
 document.getElementById("main").style.backgroundColor = chosen_color;
